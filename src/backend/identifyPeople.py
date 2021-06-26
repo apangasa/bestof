8134e4429d31abeeca253b2e3b923be3288d6e15
apangasa <60531810+apangasa@users.noreply.github.com>
2021-06-26 14:01:50 -0400

    Fix incorrect eye bounds on angled faces.

--- src/backend/identifyPeople.py
+++ src/backend/identifyPeople.py
@@ -24,6 +24,18 @@ def get_subject_bounds(face_info):
     return face_info['box']
 
 
+# def correct_eye_bounds(top_left, bottom_right):
+#     new_top_left = min(top_left[0], bottom_right[0]), min(
+#         top_left[1], bottom_right[1])
+#     new_bottom_right = max(top_left[0], bottom_right[0]), max(
+#         top_left[1], bottom_right[1])
+#     return new_top_left, new_bottom_right
+
+
+def is_left_eye_higher(face_info):
+    return face_info['keypoints']['left_eye'][1] < face_info['keypoints']['right_eye'][1]
+
+
 def get_eye_bounds(face_info):
     # Average eye diameter is roughly 23 mm: Bekerman, Inessa & Gottlieb, Paul & Vaiman, Michael. (2014). Variations in Eyeball Diameters of the Healthy Adults. Journal of ophthalmology. 2014. 503645. 10.1155/2014/503645. \
     # Average face width is roughly 147.6 mm: DU LL, Wang LM, Zhuang Z. [Measurement and analysis of human head-face dimensions]. Zhonghua Lao Dong Wei Sheng Zhi Ye Bing Za Zhi. 2008 May;26(5):266-70. Chinese. PMID: 18727867.
@@ -35,12 +47,21 @@ def get_eye_bounds(face_info):
 
     face_bounds = get_subject_bounds(face_info)
     _, _, face_width, _ = face_bounds
-    top_left = int(face_info['keypoints']['left_eye'][0] - (
-        face_width * extension)), int(face_info['keypoints']['left_eye'][1] - (face_width * extension))
-    bottom_right = int(face_info['keypoints']['right_eye'][0] + (
-        face_width * extension)), int(face_info['keypoints']['right_eye'][1] + (face_width * extension))
 
-    return top_left, bottom_right
+    left_higher = is_left_eye_higher(face_info)
+
+    if left_higher:
+        top_left = int(face_info['keypoints']['left_eye'][0] - (
+            face_width * extension)), int(face_info['keypoints']['left_eye'][1] - (face_width * extension))
+        bottom_right = int(face_info['keypoints']['right_eye'][0] + (
+            face_width * extension)), int(face_info['keypoints']['right_eye'][1] + (face_width * extension))
+    else:
+        top_left = int(face_info['keypoints']['left_eye'][0] - (
+            face_width * extension)), int(face_info['keypoints']['right_eye'][1] - (face_width * extension))
+        bottom_right = int(face_info['keypoints']['right_eye'][0] + (
+            face_width * extension)), int(face_info['keypoints']['left_eye'][1] + (face_width * extension))
+
+    return (top_left, bottom_right)
 
 
 def show_point(image, point):
@@ -49,8 +70,6 @@ def show_point(image, point):
 
 
 def show_rect(image, top_left, bottom_right):
-    # print(top_left)
-    # print(bottom_right)
     cv.rectangle(
         image, (top_left[0], top_left[1]), (bottom_right[0], bottom_right[1]), (0, 255, 0), 1)
     show_img(image)
@@ -68,7 +87,6 @@ def main():
     faces_info = detect_faces(img)
     for face in faces_info:
         eyes = get_eye_bounds(face)
-        # print(eyes)
         show_rect(img, eyes[0], eyes[1])
 
 
