3418e0daaa7ada79dc72cb1d9e439b104e56f973
apangasa <60531810+apangasa@users.noreply.github.com>
2021-07-06 17:30:29 -0400

    Update requirements

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup
 
-setup (
-    name = "bestOf",
-    version = "0.0.39",
-    description = "A module that uses machine learning to chose the best image in groups",
-    packages = ["bestOf","bestOf.backend", "bestOf.backend.saved_models", "bestOf.frontend",],
-    url = "https://github.com/apangasa/bestof",
-    install_requires = ["torch",
-    "torchvision",
-    "tensorflow",
-    "PyQt5",
-    "mtcnn",
-    "Pillow", 
-    "matplotlib", 
-    "opencv-python"],
-    include_package_data = True,
+setup(
+    name="bestOf",
+    version="0.0.50",
+    description="A module that uses machine learning to chose the best image in groups",
+    packages=["bestOf", "bestOf.backend",
+              "bestOf.backend.saved_models", "bestOf.frontend", ],
+    url="https://github.com/apangasa/bestof",
+    install_requires=["torch",
+                      "torchvision",
+                      "mediapipe",
+                      "PyQt5",
+                      "Pillow",
+                      "matplotlib",
+                      "opencv-python"],
+    include_package_data=True,
 
-    entry_points = {
+    entry_points={
         'console_scripts': ['bestOf = bestOf.frontend.frontend:main'],
     }
 
